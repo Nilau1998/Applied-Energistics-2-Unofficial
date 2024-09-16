d6e70df0458a3c49708064a97a2a105781aae46e
Nilau1998 <nico.spiske@gmail.com>
2024-09-16 21:18:54 +0200

    [minor] drawing optimizations

--- src/main/java/appeng/client/gui/widgets/GuiGraph.java
+++ src/main/java/appeng/client/gui/widgets/GuiGraph.java
@@ -1,12 +1,13 @@
 package appeng.client.gui.widgets;
 
-import appeng.client.gui.AEBaseGui;
-import net.minecraft.client.renderer.OpenGlHelper;
-import net.minecraft.client.renderer.Tessellator;
-import org.lwjgl.opengl.GL11;
-
 import java.util.Collection;
 import java.util.Random;
+
+import net.minecraft.client.renderer.OpenGlHelper;
+
+import org.lwjgl.opengl.GL11;
+
+import appeng.client.gui.AEBaseGui;
 
 public abstract class GuiGraph<COLLECTION extends Collection<?>> {
 
@@ -25,66 +26,28 @@ public abstract class GuiGraph<COLLECTION extends Collection<?>> {
     }
 
     public void drawFG(final int offsetX, final int offsetY, final int mouseX, final int mouseY) {
-        int size = graphData.size();
-        int height = this.height;
-        int lastRelativeHeight = getRelativeHeight(0, height);
-        for (int i = 0; i < size; i++) {
-            int relativeHeight = getRelativeHeight(i, height);
-            graphHost.drawHorizontalLine(offsetX + i, offsetX + i, offsetY + height - relativeHeight, this.COLOR);
-            if (Math.abs(relativeHeight - lastRelativeHeight) >= 3) {
-                graphHost.drawVerticalLine(
-                        offsetX + i,
-                        verticalBlimpStartY(offsetY, height, relativeHeight, relativeHeight - lastRelativeHeight),
-                        verticalBlimpEndY(offsetY, height, relativeHeight, relativeHeight - lastRelativeHeight),
-                        this.COLOR);
-            }
-            lastRelativeHeight = relativeHeight;
-        }
         drawGraph(offsetX, offsetY);
         renderTooltip(offsetX, offsetY, mouseX, mouseY);
     }
 
     private void drawGraph(final int offsetX, final int offsetY) {
-        float f3 = (float)(this.COLOR >> 24 & 255) / 255.0F;
-        float f = (float)(this.COLOR >> 16 & 255) / 255.0F;
-        float f1 = (float)(this.COLOR >> 8 & 255) / 255.0F;
-        float f2 = (float)(this.COLOR & 255) / 255.0F;
+        float f3 = (float) (this.COLOR >> 24 & 255) / 255.0F;
+        float f = (float) (this.COLOR >> 16 & 255) / 255.0F;
+        float f1 = (float) (this.COLOR >> 8 & 255) / 255.0F;
+        float f2 = (float) (this.COLOR & 255) / 255.0F;
 
-
-
-        Tessellator tessellator = Tessellator.instance;
         GL11.glEnable(GL11.GL_BLEND);
         GL11.glDisable(GL11.GL_TEXTURE_2D);
         OpenGlHelper.glBlendFunc(770, 771, 1, 0);
         GL11.glColor4f(f, f1, f2, f3);
-        tessellator.startDrawingQuads();
 
         int size = graphData.size();
-        int height = this.height;
-        int lastRelativeHeight = getRelativeHeight(0, height);
-
+        GL11.glBegin(GL11.GL_LINE_STRIP);
         for (int i = 0; i < size; i++) {
-            int relativeHeight = getRelativeHeight(i, height);
-            // x-axis
-            int x = offsetX + i;
-            int y = offsetY + height - relativeHeight;
-            tessellator.addVertex((double)x, (double)y + 1, 0.0D);
-            tessellator.addVertex((double)x + 1, (double)y + 1, 0.0D);
-            tessellator.addVertex((double)x + 1, (double)x + 1, 0.0D);
-            tessellator.addVertex((double)x, (double)x + 1, 0.0D);
-            // y-axis
-            if (Math.abs(relativeHeight - lastRelativeHeight) >= 3) {
-                int startY = verticalBlimpStartY(offsetY, height, relativeHeight, relativeHeight - lastRelativeHeight);
-                int endY = verticalBlimpEndY(offsetY, height, relativeHeight, relativeHeight - lastRelativeHeight);
-                tessellator.addVertex((double)x, (double)endY, 0.0D);
-                tessellator.addVertex((double)x + 1, (double)endY, 0.0D);
-                tessellator.addVertex((double)x + 1, (double)startY + 1, 0.0D);
-                tessellator.addVertex((double)x, (double)startY + 1, 0.0D);
-            }
-            lastRelativeHeight = relativeHeight;
+            GL11.glVertex2f(offsetX + i, offsetY + this.height - getRelativeHeight(i, this.height));
         }
+        GL11.glEnd();
 
-        tessellator.draw();
         GL11.glEnable(GL11.GL_TEXTURE_2D);
         GL11.glDisable(GL11.GL_BLEND);
     }
@@ -102,12 +65,7 @@ public abstract class GuiGraph<COLLECTION extends Collection<?>> {
 
     private int generateColor() {
         Random rand = new Random();
-        return getColorDecimal(
-                rand.nextInt(255),
-                rand.nextInt(255),
-                rand.nextInt(255),
-                255
-        );
+        return getColorDecimal(rand.nextInt(255), rand.nextInt(255), rand.nextInt(255), 255);
     }
 
     public static int getColorDecimal(int red, int green, int blue, int alpha) {
@@ -119,10 +77,10 @@ public abstract class GuiGraph<COLLECTION extends Collection<?>> {
     }
 
     private void renderTooltip(final int offsetX, final int offsetY, final int mouseX, final int mouseY) {
-//        int hoverIndex = offsetX + mouseX;
-//        if (hoverIndex >= 0 && hoverIndex < graphData.size()) {
-//            this.graphHost.drawTooltip(0, 0, 0, "Hey!");
-//        }
+        // int hoverIndex = offsetX + mouseX;
+        // if (hoverIndex >= 0 && hoverIndex < graphData.size()) {
+        // this.graphHost.drawTooltip(0, 0, 0, "Hey!");
+        // }
     }
 
     protected abstract int getRelativeHeight(int index, int height);
--- src/main/java/appeng/client/gui/widgets/GuiProductionStatsGraph.java
+++ src/main/java/appeng/client/gui/widgets/GuiProductionStatsGraph.java
@@ -1,13 +1,9 @@
 package appeng.client.gui.widgets;
 
+import java.util.ArrayList;
+
 import appeng.client.gui.implementations.GuiProductionStats;
 import appeng.client.gui.widgets.GuiProductionStatsPanel.PanelSide;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class GuiProductionStatsGraph {
 
@@ -27,7 +23,7 @@ public class GuiProductionStatsGraph {
     public GuiProductionStatsGraph(GuiProductionStats parent, PanelSide side) {
         this.parent = parent;
         this.side = side;
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < 3000; i++) {
             graphs.add(new GuiLongGraph(parent, GRAPH_WIDTH, GRAPH_HEIGHT));
         }
     }
@@ -37,14 +33,20 @@ public class GuiProductionStatsGraph {
     public void drawFG(int offsetX, int offsetY, int mouseX, int mouseY) {
         for (int i = 0; i < graphs.size(); i++) {
             double sinVal = (i + 1) / 25.0;
-            graphs.get(i).addData((float) (GRAPH_HEIGHT * Math.sin(sinVal * bla)));
-            graphs.get(i).drawFG(GRAPH_X_ORIGIN - (GRAPH_TEXTURE_WIDTH / 2) + 1, GRAPH_Y_ORIGIN - (GRAPH_TEXTURE_HEIGHT), mouseX, mouseY);
+            // graphs.get(i).addData((float) (GRAPH_HEIGHT * Math.sin(sinVal * bla)));
+            graphs.get(i).addData(GRAPH_HEIGHT / (1 + i));
+            graphs.get(i).drawFG(
+                    GRAPH_X_ORIGIN - (GRAPH_TEXTURE_WIDTH / 2) + 1,
+                    GRAPH_Y_ORIGIN - (GRAPH_TEXTURE_HEIGHT),
+                    mouseX,
+                    mouseY);
         }
         bla += 1;
     }
 
     public void drawBG(int offsetX, int offsetY, int mouseX, int mouseY) {
-        int offsetXAdjusted = this.side.equals(PanelSide.LEFT) ? offsetX - (GRAPH_TEXTURE_WIDTH / 2) + 1: offsetX + (GRAPH_TEXTURE_WIDTH / 2) - 1;
+        int offsetXAdjusted = this.side.equals(PanelSide.LEFT) ? offsetX - (GRAPH_TEXTURE_WIDTH / 2) + 1
+                : offsetX + (GRAPH_TEXTURE_WIDTH / 2) - 1;
         int offsetYAdjusted = offsetY - (GRAPH_TEXTURE_HEIGHT / 3);
         parent.bindTexture("guis/productionstatsgraph.png");
         parent.drawTexturedModalRect(offsetXAdjusted, offsetYAdjusted, 0, 0, GRAPH_TEXTURE_WIDTH, GRAPH_TEXTURE_HEIGHT);
